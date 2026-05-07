b13e474aae3a7d3ab6145406f974b84cce00a74d
unnatiprof <unnatiprofessional@gmail.com>
2026-05-07 09:24:57 +0530

    Added the entity file Transaction.java

--- fintrack/src/main/java/com/unnati/fintrack/entity/Transaction.java
+++ fintrack/src/main/java/com/unnati/fintrack/entity/Transaction.java
@@ -7,21 +7,5 @@ import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Transaction {
-    private Long id;
-    private String title;
-    private String description;
-    private BigDecimal amount;
-    private TransactionType type;
-    private PaymentMode paymentMode;
-    private LocalDate transactionDate;
-
-    @ManyToOne
-    private Category category;
-
-    @ManyToOne
-    private Account account;
-
-    @ManyToOne
-    private User user;
-    //comment to check github updates.
+    
 }

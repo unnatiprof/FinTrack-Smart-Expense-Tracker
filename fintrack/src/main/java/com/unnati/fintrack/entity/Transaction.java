a15f7a34387e66fe6d8438c3a7c78a4c49c0ed20
unnatiprof <unnatiprofessional@gmail.com>
2026-05-07 10:17:25 +0530

    Added comments in transaction entity for understanding.

--- fintrack/src/main/java/com/unnati/fintrack/entity/Transaction.java
+++ fintrack/src/main/java/com/unnati/fintrack/entity/Transaction.java
@@ -1,24 +1,24 @@
-package com.unnati.fintrack.entity;
+package com.unnati.fintrack.entity;  // package
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.math.BigDecimal;  // for money precision
+import java.time.LocalDate;  // best for local date
 
-import com.unnati.fintrack.enums.PaymentMode;
-import com.unnati.fintrack.enums.TransactionStatus;
-import com.unnati.fintrack.enums.TransactionType;
+import com.unnati.fintrack.enums.PaymentMode;  // imported payment mode enum.
+import com.unnati.fintrack.enums.TransactionStatus;  // imported transaction status enum.
+import com.unnati.fintrack.enums.TransactionType;  // imported transaction type enum.
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Entity;  // for importing @entity annotation.
+import jakarta.persistence.EnumType;  // used for how to save an enum in a DB.
+import jakarta.persistence.Enumerated;  // used to import @Enumerated annotation.
+import jakarta.persistence.GeneratedValue; // for importing @GeneratedValue annotation.
+import jakarta.persistence.GenerationType; // used to tell the generation strategy for id.
+import jakarta.persistence.Id; // for importing @Id annotation.
+import jakarta.persistence.Table;  // for importing @Table annotation.
+import lombok.AllArgsConstructor;  //  for importing @AllArgsContructor.
+import lombok.Builder;  // for importing @Builder.
+import lombok.Getter;  // for importing @Getter
+import lombok.NoArgsConstructor;  //  for importing @NoArgsContructor.
+import lombok.Setter;  //for importing @Setter.
 
 @Getter
 @Setter

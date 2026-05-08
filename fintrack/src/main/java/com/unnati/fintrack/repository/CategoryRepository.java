5409856cbe45fe3a6f63e3a3d82c9b02d4581aab
unnatiprof <unnatiprofessional@gmail.com>
2026-05-09 01:22:18 +0530

    Edited CategoryRepository.

--- fintrack/src/main/java/com/unnati/fintrack/repository/CategoryRepository.java
+++ fintrack/src/main/java/com/unnati/fintrack/repository/CategoryRepository.java
@@ -1,5 +1,24 @@
 package com.unnati.fintrack.repository;
 
-public interface CategoryRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unnati.fintrack.entity.Category;
+import com.unnati.fintrack.enums.CategoryStatus;
+import com.unnati.fintrack.enums.CategoryType;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long>
+{
+      List<Category> findByCategory(CategoryType type);
+      
+      List<Category> findByStatus(CategoryStatus status);
+
+      Optional<Category> findByNameIgnoreCase(String name);
+
+      List<Category> findByNameContainingIgnoreCase(String keyword);
+
+      boolean existsByNameIgnoreCase(String name);
 }

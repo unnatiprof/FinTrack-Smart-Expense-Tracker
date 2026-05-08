ee8b0328878f7be16c7c44aa2d9daf063cf261fd
unnatiprof <unnatiprofessional@gmail.com>
2026-05-08 22:16:37 +0530

    Completed the TransactionRepository interface.

--- fintrack/src/main/java/com/unnati/fintrack/repository/TransactionRepository.java
+++ fintrack/src/main/java/com/unnati/fintrack/repository/TransactionRepository.java
@@ -1,5 +1,28 @@
 package com.unnati.fintrack.repository;
 
-public interface TransactionRepository {
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unnati.fintrack.entity.Transaction;
+import com.unnati.fintrack.enums.PaymentMode;
+import com.unnati.fintrack.enums.TransactionStatus;
+import com.unnati.fintrack.enums.TransactionType;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByType(TransactionType type);
+
+    List<Transaction> findByCategory(String category);
+
+    List<Transaction> findByAccount(String account);
+
+    List<Transaction> findByPaymentMode(PaymentMode paymentMode);
+
+    List<Transaction> findByTransactionStatus(TransactionStatus transactionStatus);
+
+    List<Transaction> findByTransactionDate(LocalDate transactionDate);
+
+    List<Transaction> findByTransactionDateBetween(LocalDate startDate, LocalDate endDate);
 }

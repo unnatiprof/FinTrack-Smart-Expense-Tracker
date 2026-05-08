a5dc2b374ecebb67d7c542febe6196e40d608a4a
unnatiprof <unnatiprofessional@gmail.com>
2026-05-08 23:02:26 +0530

    Added the interface AccountRepository.

--- fintrack/src/main/java/com/unnati/fintrack/repository/AccountRepository.java
+++ fintrack/src/main/java/com/unnati/fintrack/repository/AccountRepository.java
@@ -1,5 +1,13 @@
 package com.unnati.fintrack.repository;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unnati.fintrack.entity.Account;
+import com.unnati.fintrack.enums.AccountType;
+import com.unnati.fintrack.enums.AccountStatus;
+import com.unnati.fintrack.enums.CurrencyType;
+
 public interface AccountRepository {
 
 }

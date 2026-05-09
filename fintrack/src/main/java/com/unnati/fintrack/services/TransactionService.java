8072a85bfda6e683afb455298c604424b4d15740
unnatiprof <unnatiprofessional@gmail.com>
2026-05-09 05:34:38 +0530

    Added the file TransactionService.

--- fintrack/src/main/java/com/unnati/fintrack/services/TransactionService.java
+++ fintrack/src/main/java/com/unnati/fintrack/services/TransactionService.java
@@ -1,5 +1,18 @@
 package com.unnati.fintrack.services;
 
+import java.util.List;
+
+import com.unnati.fintrack.entity.Transaction;
+
 public interface TransactionService {
 
+    List<Transaction> findAll();
+
+    Transaction findById(Long id);
+
+    Transaction save(Transaction transaction);
+
+    Transaction update(Long id, Transaction transaction);
+
+    void deleteById(Long id);
 }

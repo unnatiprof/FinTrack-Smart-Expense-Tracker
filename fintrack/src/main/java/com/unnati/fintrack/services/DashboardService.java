774e8827b5b6fe94dc96fafb011afb6e9099c1bf
unnatiprof <unnatiprofessional@gmail.com>
2026-05-09 05:53:38 +0530

    Added the file DashboardService.

---
+++ fintrack/src/main/java/com/unnati/fintrack/services/DashboardService.java
@@ -0,0 +1,8 @@
+package com.unnati.fintrack.services;
+
+import com.unnati.fintrack.dto.response.DashboardSummaryDto;
+
+public interface DashboardService {
+
+    DashboardSummaryDto getSummary();
+}

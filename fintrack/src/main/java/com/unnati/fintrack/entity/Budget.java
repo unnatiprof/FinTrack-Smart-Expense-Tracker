9351a9e7c34582f25181008f4228b5834c94598b
unnatiprof <unnatiprofessional@gmail.com>
2026-09-06 19:51:48 +0530

    Created Budget Entity File.

---
+++ fintrack/src/main/java/com/unnati/fintrack/entity/Budget.java
@@ -0,0 +1,50 @@
+package com.unnati.fintrack.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.unnati.fintrack.enums.BudgetStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "budgets")
+public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String budgetName;
+
+    private String category;
+
+    private Integer month;
+
+    private Integer year;
+
+    private BigDecimal limitAmount;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    private BudgetStatus status;
+}

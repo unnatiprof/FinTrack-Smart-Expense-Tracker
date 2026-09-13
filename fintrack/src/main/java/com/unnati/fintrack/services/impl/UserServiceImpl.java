c1a25927cdd87ed34e42b723f268392d3cbe1d82
unnatiprof <unnatiprofessional@gmail.com>
2026-09-13 19:13:14 +0530

    Edited user service implementation file.

--- fintrack/src/main/java/com/unnati/fintrack/services/impl/UserServiceImpl.java
+++ fintrack/src/main/java/com/unnati/fintrack/services/impl/UserServiceImpl.java
@@ -3,9 +3,11 @@ package com.unnati.fintrack.services.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import com.unnati.fintrack.entity.User;
+import com.unnati.fintrack.events.UserRegisteredEvent;
 import com.unnati.fintrack.repository.UserRepository;
 import com.unnati.fintrack.services.UserService;
 
@@ -14,8 +16,14 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    private final ApplicationEventPublisher eventPublisher;
+
+    public UserServiceImpl(
+            UserRepository userRepository,
+            ApplicationEventPublisher eventPublisher) {
+
         this.userRepository = userRepository;
+        this.eventPublisher = eventPublisher;
     }
 
     @Override
@@ -26,7 +34,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Long id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+                .orElseThrow(() ->
+                        new RuntimeException(
+                                "User not found with id: " + id
+                        ));
     }
 
     @Override
@@ -36,15 +47,32 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User save(User user) {
+
         if (userRepository.existsByEmail(user.getEmail())) {
-            throw new RuntimeException("Email already exists: " + user.getEmail());
+
+            throw new RuntimeException(
+                    "Email already exists: "
+                            + user.getEmail()
+            );
         }
 
-        return userRepository.save(user);
+        User savedUser =
+                userRepository.save(user);
+
+        eventPublisher.publishEvent(
+                new UserRegisteredEvent(
+                        savedUser.getId(),
+                        savedUser.getName(),
+                        savedUser.getEmail()
+                )
+        );
+
+        return savedUser;
     }
 
     @Override
     public User update(Long id, User user) {
+
         User existingUser = findById(id);
 
         existingUser.setName(user.getName());
@@ -58,7 +86,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteById(Long id) {
+
         User existingUser = findById(id);
+
         userRepository.delete(existingUser);
     }
 

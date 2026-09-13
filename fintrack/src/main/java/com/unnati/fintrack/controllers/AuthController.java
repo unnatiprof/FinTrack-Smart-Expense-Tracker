e4e221ce68599a1cd1876f793996bbfee68533b0
unnatiprof <unnatiprofessional@gmail.com>
2026-09-13 19:12:52 +0530

    Edited AuthController File.

--- fintrack/src/main/java/com/unnati/fintrack/controllers/AuthController.java
+++ fintrack/src/main/java/com/unnati/fintrack/controllers/AuthController.java
@@ -1,5 +1,44 @@
 package com.unnati.fintrack.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.unnati.fintrack.dto.request.LoginRequest;
+import com.unnati.fintrack.dto.request.RegisterRequest;
+import com.unnati.fintrack.dto.response.LoginResponse;
+import com.unnati.fintrack.services.AuthService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+@Validated
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<LoginResponse> register(
+            @Valid @RequestBody RegisterRequest request) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @Valid @RequestBody LoginRequest request) {
+
+        return ResponseEntity.ok(authService.login(request));
+    }
 }

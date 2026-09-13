cba429e4f1aac8480238b683b2f756860f999f23
unnatiprof <unnatiprofessional@gmail.com>
2026-09-13 19:19:15 +0530

    Edited Auth Service file.

--- fintrack/src/main/java/com/unnati/fintrack/services/AuthService.java
+++ fintrack/src/main/java/com/unnati/fintrack/services/AuthService.java
@@ -1,5 +1,12 @@
 package com.unnati.fintrack.services;
 
-public class AuthService {
+import com.unnati.fintrack.dto.request.LoginRequest;
+import com.unnati.fintrack.dto.request.RegisterRequest;
+import com.unnati.fintrack.dto.response.LoginResponse;
 
+public interface AuthService {
+
+    LoginResponse register(RegisterRequest request);
+
+    LoginResponse login(LoginRequest request);
 }

e476e6cbed55b3449e73856fae75798a8a41a2d2
unnatiprof <unnatiprofessional@gmail.com>
2026-09-13 19:15:36 +0530

    Created Transaction Event creator.

---
+++ fintrack/src/main/java/com/unnati/fintrack/events/TransactionCreatedEvent.java
@@ -0,0 +1,23 @@
+package com.unnati.fintrack.events;
+
+import java.math.BigDecimal;
+
+import com.unnati.fintrack.enums.TransactionType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TransactionCreatedEvent {
+
+    private Long transactionId;
+
+    private String title;
+
+    private BigDecimal amount;
+
+    private TransactionType type;
+
+    private String category;
+}

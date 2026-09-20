0b855eac7ad4cccfcb2d132ae9fbdf4b4ed67887
unnatiprof <unnatiprofessional@gmail.com>
2026-09-20 13:35:13 +0530

    Edited new file.

---
+++ fintrack/src/main/java/com/unnati/fintrack/security/JwtService.java
@@ -0,0 +1,85 @@
+package com.unnati.fintrack.security;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    private final SecretKey secretKey;
+    private final long expiration;
+
+    public JwtService(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") long expiration) {
+
+        this.secretKey = Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8));
+
+        this.expiration = expiration;
+    }
+
+    public String generateToken(Long userId, String email, String role) {
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+                .subject(email)
+                .claim("userId", userId)
+                .claim("role", role)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+
+        return getClaims(token).getSubject();
+    }
+
+    public Long extractUserId(String token) {
+
+        return getClaims(token)
+                .get("userId", Long.class);
+    }
+
+    public boolean isTokenValid(String token, String email) {
+
+        try {
+            String tokenEmail = extractEmail(token);
+
+            return tokenEmail.equals(email)
+                    && !isTokenExpired(token);
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        return getClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}

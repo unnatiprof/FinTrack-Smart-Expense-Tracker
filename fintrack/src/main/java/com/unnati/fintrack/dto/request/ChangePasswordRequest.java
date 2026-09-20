10b5e581f4b02dba20694a246c0d4d5942e2be35
unnatiprof <unnatiprofessional@gmail.com>
2026-09-20 13:34:31 +0530

    Edited new file.

---
+++ fintrack/src/main/java/com/unnati/fintrack/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,21 @@
+package com.unnati.fintrack.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChangePasswordRequest {
+
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 6, message = "New password must be at least 6 characters")
+    private String newPassword;
+
+    @NotBlank(message = "Confirm password is required")
+    private String confirmPassword;
+}

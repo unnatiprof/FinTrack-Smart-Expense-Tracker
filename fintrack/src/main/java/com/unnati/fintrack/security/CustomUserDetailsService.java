8c5ed5ee90e410bc89d9c38a47345be7a08c4128
unnatiprof <unnatiprofessional@gmail.com>
2026-09-20 13:34:47 +0530

    Edited new file.

---
+++ fintrack/src/main/java/com/unnati/fintrack/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.unnati.fintrack.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.unnati.fintrack.entity.User;
+import com.unnati.fintrack.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(user.getRole().name())
+                .build();
+    }
+}

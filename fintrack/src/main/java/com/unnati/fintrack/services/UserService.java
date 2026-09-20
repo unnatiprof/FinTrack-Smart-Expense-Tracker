8cec06f50f0d6f8863e6f8947f66f45a82ec8973
unnatiprof <unnatiprofessional@gmail.com>
2026-09-20 13:32:24 +0530

    Edited new file.

--- fintrack/src/main/java/com/unnati/fintrack/services/UserService.java
+++ fintrack/src/main/java/com/unnati/fintrack/services/UserService.java
@@ -3,6 +3,9 @@ package com.unnati.fintrack.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.unnati.fintrack.dto.request.ChangePasswordRequest;
+import com.unnati.fintrack.dto.request.ProfileUpdateRequest;
+import com.unnati.fintrack.dto.response.ProfileResponse;
 import com.unnati.fintrack.entity.User;
 
 public interface UserService {
@@ -20,4 +23,14 @@ public interface UserService {
     void deleteById(Long id);
 
     boolean existsByEmail(String email);
+
+    ProfileResponse getProfile(String email);
+
+    ProfileResponse updateProfile(
+            String email,
+            ProfileUpdateRequest request);
+
+    void changePassword(
+            String email,
+            ChangePasswordRequest request);
 }

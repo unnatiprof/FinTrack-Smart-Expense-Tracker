6e021e163f1a89f9099d35b2594dec19603c61bf
unnatiprof <unnatiprofessional@gmail.com>
2026-09-20 13:35:53 +0530

    Edited new file.

---
+++ fintrack/src/main/java/com/unnati/fintrack/dto/request/ProfileUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.unnati.fintrack.dto.request;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProfileUpdateRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Enter a valid email")
+    private String email;
+
+    private String currency;
+
+    private BigDecimal monthlyIncome;
+}
